a437c095ff64bdaf5bbf7fffb61f8ccc393c5c98
jdav892 <jadavila9@gmail.com>
2024-06-29 01:30:01 -0400

    ui layout finished

--- main.py
+++ main.py
@@ -24,15 +24,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="100daysOfPy/pomodoro/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold" ) )
-canvas.pack()
+canvas.grid(column=1, row=1)
 
-timer_label = Label(text="Timer")
-timer_label.pack()
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "bold"))
+timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", command=start_button_pressed)
-start_button.pack()
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
 
-reset_button = Button(text="Reset", command=reset_button_pressed)
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 
 
 
